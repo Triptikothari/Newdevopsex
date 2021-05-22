f8175d0437a5b34ff806dfdaddc4a3fdee607a2e
Tripti Kothari <triptikothari1999@gmail.com>
2021-05-22 16:51:28 +0530

    commiting

---
+++ src/main/java/com/example/Newdevopsex/NewdevopsexApplication.java
@@ -0,0 +1,13 @@
+package com.example.Newdevopsex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewdevopsexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewdevopsexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Newdevopsex/NewdevopsexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Newdevopsex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewdevopsexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
